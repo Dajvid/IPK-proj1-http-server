a65ec5a368ca7b3bc773e23d24030cc0799d47c5
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2019-02-17 23:02:09 +0100

    server.c establish connection

--- server.c
+++ server.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-    int port, sockfd;
+    int port, server_socket, rc, comm_socket;
+    unsigned int client_len;
     char *endptr;
+    struct sockaddr_in sa, sa_client;
 
     /* argument parsing */
     if (argc < 2) {
@@ -22,12 +27,38 @@ int main(int argc, char **argv)
     }
 
     /* socket creation */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd <= 0) {
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("ERROR in socket");
         return EXIT_FAILURE;
     }
+    printf("%d\n", port);
 
+    memset((char *)&sa, 0, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+    sa.sin_port = htons(port);
+
+    if ((rc = bind(server_socket, (const struct sockaddr *)&sa, sizeof(sa))) < 0) {
+        perror("ERROR: bind");
+        return(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 1) < 0) {
+        perror("ERROR: listen");
+        return(EXIT_FAILURE);
+    }
+
+    while (1) {
+        comm_socket = accept(server_socket, (struct sockaddr *)&sa_client, &client_len);
+        printf("comm_socket - %d\n", comm_socket);
+        if (comm_socket > 0) {
+            char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 10\n\nIt's alive";
+            char buffer[1024] = {0};
+            read(comm_socket , buffer, 1024);
+            printf("%s\n", buffer);
+            write(comm_socket, hello, strlen(hello));
+        }
+    }
 
 
     return EXIT_SUCCESS;
